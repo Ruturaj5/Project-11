11e8e287b0698bd95aa757d3e4f1b2ef7f8d2ffe
rutur <rutur@RUTURAJ>
2024-01-13 08:23:03 +0530

    By Ruturaj

---
+++ Git PushPull 11th Project 13 January/src/com/cjc/Demo11/Git_Demo13.java
@@ -0,0 +1,18 @@
+package com.cjc.Demo11;
+
+public class Git_Demo13 
+{
+	public void m11() 
+	{
+	  System.out.println("Method m11");
+	}
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("Main Method m11");
+		Git_Demo13  gd11 = new Git_Demo13 ();
+		gd11.m11();
+		
+	}
+
+}
